9dedc7d2a2dd9b319b577a1f0dd872c6ad76d082
sirawat <sirawat_top@cmu.ac.th>
2021-06-05 22:10:33 +0700

    comit2

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById('ok').addEventListener('click',function(e){
-    alert('ok is clicked')
+document.getElementById('ok').addEventListener('click',function(){
+    alert('OK')
+    
+})
+document.getElementById('cancel').addEventListener('click',function(){
+    alert('CANCEL')
+    
+})
+document.getElementById('no').addEventListener('click',function(){
+    alert('NO')
+    
+})
+document.getElementById('cancel').addEventListener('mousemove',function(a){
+    var x = a.clientX;
+    var y = a.clientY;
+    console.log('x = '+x+' y = '+y)
 })
